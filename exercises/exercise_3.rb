fd27bb9edbca4510ba2a2ead9f74b22764135229
Liuming <lfan.canada@gmail.com>
2021-05-25 15:44:37 +0000

    delete store and count number of stores

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,9 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+# Find and delete store3 from database:
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+
+#Verify store3 has been deleted by checking number of stores:
+puts "Number of stores in the database: #{Store.count}"  
