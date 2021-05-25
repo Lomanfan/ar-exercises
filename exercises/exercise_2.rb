3c5dd4163d785effb944543fe33bfc0bd15b275d
Liuming <lfan.canada@gmail.com>
2021-05-25 15:43:45 +0000

    find store by id and update store name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,11 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+#find store by id from database; create instance variables: @store1, @store2:
+@store1 = Store.find_by(id: 1);
+@store2 = Store.find_by(id: 2);
+
+#change store instance name of store1
+@store1.name = 'LHL Outdoors';
+@store1.save
+puts @store1.name;
