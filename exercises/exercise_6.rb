0c1b7dcf307136376ff7dc4ddfe50325ae014a1f
Liuming <lfan.canada@gmail.com>
2021-05-25 15:51:34 +0000

    add new employees to stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# @store1 = Store.find_by(id: 1)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Greg", last_name: "Paterson", hourly_rate: 60)
+
+# @store2 = Store.find_by(id: 2)
+@store2.employees.create(first_name: "Jimmy", last_name: "Kool", hourly_rate: 50)
+@store2.employees.create(first_name: "Lisa", last_name: "Barkman", hourly_rate: 50)
