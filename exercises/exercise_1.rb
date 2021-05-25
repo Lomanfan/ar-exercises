ed69bb1eeb134a9b025f10a9dcb1f63c4971feab
Liuming <lfan.canada@gmail.com>
2021-05-25 15:42:54 +0000

    assign value to instance variables

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,9 +4,9 @@ puts "Exercise 1"
 puts "----------"
 
 #create 3 stores in the database:
-Store.create(name: "Burnaby", annual_reveneue: 3000000, mens_apparel: true, womens_apparel: true);
-Store.create(name: "Richmond", annual_reveneue: 1260000, mens_apparel: false, womens_apparel: true);
-Store.create(name: "Gastown", annual_reveneue: 190000, mens_apparel: true, womens_apparel: false);
+@store1 = Store.create(name: "Burnaby", annual_revenue: 3000000, mens_apparel: true, womens_apparel: true)
+@store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+@store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 #puts to print number of stores in the database:
 puts "Number of stores in the database: #{Store.count}"
