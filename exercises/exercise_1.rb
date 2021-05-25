84049f159390dfa965b2d3fa9c1b2a32c830d0ac
Liuming <lfan.canada@gmail.com>
2021-05-25 05:42:21 +0000

    create stores in db, print number of stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,10 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+#create 3 stores in the database:
+Store.create(name: "Burnaby", annual_reveneue: 3000000, mens_apparel: true, womens_apparel: true);
+Store.create(name: "Richmond", annual_reveneue: 1260000, mens_apparel: false, womens_apparel: true);
+Store.create(name: "Gastown", annual_reveneue: 190000, mens_apparel: true, womens_apparel: false);
+
+#puts to print number of stores in the database:
+puts "Number of stores in the database: #{Store.count}"
